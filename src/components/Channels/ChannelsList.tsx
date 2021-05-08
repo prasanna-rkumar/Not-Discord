ff888f983dde490ec67813006a0cdef6bde1b1ed
Prasanna Kumar <prasannasrk07@gmail.com>
2021-05-08 23:32:37 +0530

    🚨 unused imports

--- src/components/Channels/ChannelsList.tsx
+++ src/components/Channels/ChannelsList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { AppContext } from "../../contexts/AppContext";
 import { discordFirestore } from "../../firebase";
