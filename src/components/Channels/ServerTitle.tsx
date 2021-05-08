693ce26a179bf9fdd1d8c611e9fccb95ccc8d1bd
Prasanna Kumar <prasannasrk07@gmail.com>
2021-05-08 20:42:09 +0530

    🩹 FIX: Channel name

--- src/components/Channels/ServerTitle.tsx
+++ src/components/Channels/ServerTitle.tsx
@@ -7,7 +7,7 @@ const ServerTitle = () => {
   return (
     <div className="border-b-2 box-border border-gray-darkest h-12 flex justify-between items-center px-4 gap-1">
       <h4 className="text-white text-base leading-5 font-semibold whitespace-nowrap overflow-ellipsis overflow-hidden">
-        #{selectedServer?.data().name}
+        {selectedServer?.data().name}
       </h4>
     </div>
   );
--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,5 +1,19 @@
+import { useContext } from "react";
+import { BiHash } from "react-icons/bi";
+import { AppContext } from "../contexts/AppContext";
+
 const TopNavbar = () => {
-  return <nav className="h-12 box-content w-full border-b-2 border-gray-darkest"></nav>;
+  const { selectedChannel } = useContext(AppContext);
+  return (
+    <nav className="h-12 box-content w-full border-b-2 border-gray-darkest flex items-center px-4">
+      {selectedChannel && (
+        <>
+          <BiHash size="24" className=" text-white-muted mr-2" />
+          <span className="text-white font-semibold">{selectedChannel.data().name}</span>
+        </>
+      )}
+    </nav>
+  );
 };
 
 export default TopNavbar;
